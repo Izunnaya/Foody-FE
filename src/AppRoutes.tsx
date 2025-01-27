37c5b1b03bf15e26f79f6a7645f45dc70788ff60
Izunnaya <ugorjimicheal.um@gmail.com>
2025-01-27 20:56:30 +0100

    Frontend setup and home layout partial completion

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import Layout from "./layouts/layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/user-profile" element={<span>user profile</span>}></Route>
+      <Route path="*" element={<Navigate to="/" />}></Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+const MainNav = () => {
+  return (
+    <span className="flex space-x-2 items-center">
+      <Link to="/order-status" className="font-bold hover:text-orange-500">
+        Order Status
+      </Link>
+
+      <Button
+        variant="ghost"
+        className="font-bold hover:text-orange-500 hover:bg-white"
+      >
+        Log In
+      </Button>
+    </span>
+  );
+};
+
+export default MainNav;
