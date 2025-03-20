6c47897d56d10d8a2f29a3058d3485ac9c850c06
Izunnaya <ugorjimicheal.um@gmail.com>
2025-03-20 12:47:13 +0100

    MyUserApi API custom hook created

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,3 +1,5 @@
+import { useMutation } from "react-query";
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 //define the type of the variables to be used in the actual request function
@@ -6,19 +8,33 @@ type CreateUserRequest = {
   email: string;
 };
 
-// The hook to make the actual request to the back end
-export const useCreateMyUser = async (user: CreateUserRequest) => {
-  const response = await fetch(`${API_BASE_URL}/api/my/user`, {
-    method: "POST",
-    headers: {
-      "content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
+// This is the custom hook to make the actual request to the backend to create a user
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-  if (!response.ok) {
-    throw new Error("failed to create user");
-  }
+    if (!response.ok) {
+      throw new Error("user not created");
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isSuccess,
+    isError,
+  };
 };
-
-// Strepp
